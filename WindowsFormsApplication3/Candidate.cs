5b9d6d85c91f6b9c4b6bf92e13fb4b96a160a876
TheEnjoy <san163ky@mail.ru>
2016-04-22 20:01:17 +0300

    22:01 Все работает

--- WindowsFormsApplication3/Candidate.cs
+++ WindowsFormsApplication3/Candidate.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication3
 {
+    [Flags]
+    public enum Candidate_Properties
+    {
+        HardWork = 1,
+        Strong = 1,
+        Smog = -1,
+        Small = -1
+    }
+
+
     class Candidate
     {
+        string nameOfCandidate;
+        public Candidate_Properties properties;
+
+        public Candidate()
+        {
+            NameOfCandidate = "";
+            Properties = new Candidate_Properties();
+        }
+
+        public Candidate(string nameOfCandidate, Candidate_Properties proper)
+        {
+            NameOfCandidate = nameOfCandidate;
+            Properties = proper;
+        }
+
+        public string NameOfCandidate
+        {
+            get { return NameOfCandidate; }
+            set { value = NameOfCandidate; }
+        }
+        public Candidate_Properties Properties
+        {
+            get { return Properties; }
+            set { value = Properties; }
+        }
     }
+
+
 }
